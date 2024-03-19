c2bec513fa3406800af8e9333828e13a626b3c89
chmuhammadasim <boybrave82@gmail.com>
2024-03-19 16:20:51 +0500

    first commit

---
+++ contracts/ETHDaddy.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract ETHDaddy is ERC721{
+    uint256 public maxSupply;
+    struct Domain{
+        string name;
+        uint256 cost;
+        bool isOwned;
+    }
+    mapping(uint256 => Domain) public domains;
+    address public owner;
+    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol){
+        owner = msg.sender;
+    }
+    function list(string memory _name,uint256 _cost) public {
+        require(msg.sender == owner, "Only owner can list");
+        maxSupply = maxSupply + 1;
+        domains[maxSupply] = Domain(_name, _cost, false);
+    }
+}
---
+++ test/ETHDaddy.js
@@ -0,0 +1,51 @@
+const { expect } = require("chai")
+const { ethers } = require("hardhat")
+
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
+describe("ETHDaddy", () => {
+  let ethDaddy
+  let deployer, owner1
+
+  const Name = 'Eth Daddy'
+  const Symbol = 'ETHD'
+
+  beforeEach(async () => {
+    ///setup accounts
+    [deployer, owner1] = await ethers.getSigners();
+    ///deploy contract
+    const ETHDaddy = await ethers.getContractFactory('ETHDaddy')
+     ethDaddy = await ETHDaddy.deploy('Eth Daddy','ETHD')
+    ///list domains
+    const transactions = await ethDaddy.connect(deployer).list("jack.eth",tokens(10))
+    await transactions.wait()
+  })
+  describe("Deployment", () => {
+    it('has a name',async () => {
+      let result = await ethDaddy.name()
+      expect(result).to.equal(Name)
+    })
+    it('has a symbol',async () => {
+      let result = await ethDaddy.symbol()
+      expect(result).to.equal(Symbol)
+    })
+    it('has a Owner',async () => {
+      let result = await ethDaddy.owner()
+      expect(result).to.equal(deployer.address)
+    })
+    it('Returns the max Supply',async () => {
+      let result = await ethDaddy.maxSupply()
+      expect(result).to.equal(1)
+    })
+  })
+  describe("domain",()=>{
+    it("Returns domain attributes",async () => {
+      let domain = await ethDaddy.domains(1);
+      expect(domain.name).to.be.equal("jack.eth")
+      expect(domain.cost).to.be.equal(tokens(10) )
+      expect(domain.isOwned).to.be.equal(false)
+    })
+  })
+})
