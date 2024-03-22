a984b902ff15d20c814d0b6ded463fa07682c741
chmuhammadasim <boybrave82@gmail.com>
2024-03-22 20:53:19 +0500

    Update ETHDaddy contract and Domain component

--- contracts/ETHDaddy.sol
+++ contracts/ETHDaddy.sol
@@ -1,13 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
+
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract ETHDaddy is ERC721{
-
+contract ETHDaddy is ERC721 {
     uint256 public maxSupply;
     uint256 public totalSupply;
-    
-    struct Domain{
+    address public owner;
+
+    struct Domain {
         string name;
         uint256 cost;
         bool isOwned;
@@ -15,43 +16,44 @@ contract ETHDaddy is ERC721{
 
     mapping(uint256 => Domain) domains;
 
-    modifier onlyOwner(){
+    modifier onlyOwner() {
         require(msg.sender == owner);
         _;
     }
 
-    address public owner;
-
-    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol){
+    constructor(string memory _name, string memory _symbol)
+        ERC721(_name, _symbol)
+    {
         owner = msg.sender;
     }
 
-    function list(string memory _name,uint256 _cost) public onlyOwner{
-        maxSupply = maxSupply + 1;
+    function list(string memory _name, uint256 _cost) public onlyOwner {
+        maxSupply++;
         domains[maxSupply] = Domain(_name, _cost, false);
     }
 
-    function mint(uint256 _id)public payable {
+    function mint(uint256 _id) public payable {
         require(_id != 0);
         require(_id <= maxSupply);
         require(domains[_id].isOwned == false);
+        require(msg.value >= domains[_id].cost);
 
         domains[_id].isOwned = true;
-        totalSupply = totalSupply + 1;
+        totalSupply++;
 
-        _safeMint(msg.sender,_id);
+        _safeMint(msg.sender, _id);
     }
 
-    function getDomains(uint256 _id) public view returns(Domain memory) {
+    function getDomain(uint256 _id) public view returns (Domain memory) {
         return domains[_id];
     }
 
-    function getBalance() public view returns(uint256){
+    function getBalance() public view returns (uint256) {
         return address(this).balance;
     }
-    
-    function withDraw() public onlyOwner(){
-        (bool success,) = owner.call{value:address(this).balance}("");
+
+    function withdraw() public onlyOwner {
+        (bool success, ) = owner.call{value: address(this).balance}("");
         require(success);
     }
 }
